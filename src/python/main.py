28869b3fcbe0afbcc44f185e8f663e8bc5b3da6c
Wesley Stagg <wes.stagg@gmail.com>
2025-10-17 10:23:02 +0100

    first draft at mapping satellites

--- src/python/main.py
+++ src/python/main.py
@@ -1,48 +1,74 @@
+import matplotlib
+matplotlib.use('TkAgg') # Set interactive backend first
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import satelliteTracker
 
-config = satelliteTracker.Config()
+# Create the figure and axis FIRST
+fig = plt.figure(figsize=(12, 8))
+ax = fig.add_subplot(111)
 
+# Create basemap
+m = Basemap(projection='robin', lon_0=0, resolution='c', ax=ax)
+m.drawcoastlines()
+m.fillcontinents(color='coral', lake_color='aqua')
+m.drawparallels(np.arange(-90., 120., 30.))
+m.drawmeridians(np.arange(0., 360., 60.))
+m.drawmapboundary(fill_color='aqua')
+
+config = satelliteTracker.Config()
 config.read()
 
 dataReceiver = satelliteTracker.DataReceiver(config)
 
-spaceStationID = 25544
+sat_plots = []
 
-satPos = dataReceiver.getSatellitePosition(spaceStationID)
+def init():
 
-for pos in satPos.positionData:
-    print(pos.altKm)
-
-satsAbove = dataReceiver.getSatellitesAbove(satelliteTracker.SatelliteCategory.All)
-
-for sat in satsAbove.satellitesAbove:
-    print(sat.satName)
-
-
-
-# import matplotlib
-# matplotlib.use('TkAgg')  # Set interactive backend first
-
-# from mpl_toolkits.basemap import Basemap
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-# import satelliteTracker
-# # lon_0, lat_0 are the center point of the projection.
-# # resolution = 'l' means use low resolution coastlines.
-
-# config = satelliteTracker.Config()
-
-# config.read()
-
-# m = Basemap(projection='ortho',lon_0 = config.getConfigValues().observerLat, lat_0 = config.getConfigValues().observerLon, resolution = 'l')
-# m.drawcoastlines()
-# m.fillcontinents(color='coral',lake_color='aqua')
-# # draw parallels and meridians.
-# m.drawparallels(np.arange(-90.,120.,30.))
-# m.drawmeridians(np.arange(0.,420.,60.))
-# m.drawmapboundary(fill_color='aqua') 
-# plt.title("Full Disk Orthographic Projection")
-# plt.show()
+    satsAbove = dataReceiver.getSatellitesAbove(satelliteTracker.SatelliteCategory.ISS)
+    print(satsAbove.transactionCount)
+    for sat in satsAbove.satellitesAbove:
+        xpt, ypt = m(sat.lon, sat.lat)
+        plot, = m.plot(xpt, ypt, 'bo', markersize=8)
+        sat_plots.append(plot)
+        #sat_names.append(text)
+    
+    return sat_plots
 
 
+def update(frame):
+
+    # Clear previous satellite markers
+    for plot in sat_plots:
+        plot.remove()
+        
+    sat_plots.clear()
+    
+    # for name in sat_names:
+    #     name.remove()
+    # sat_names.clear()
+    
+    # Get and plot new positions
+    satsAbove = dataReceiver.getSatellitesAbove(satelliteTracker.SatelliteCategory.ISS)
+    print(satsAbove.transactionCount)
+    for sat in satsAbove.satellitesAbove:
+        xpt, ypt = m(sat.lon, sat.lat)
+        plot, = m.plot(xpt, ypt, 'bo', markersize=8)
+        sat_plots.append(plot)
+        #sat_names.append(text)
+    
+    return sat_plots
+
+plt.title("Satellite Tracker")
+
+ani = animation.FuncAnimation(
+    fig,
+    update,
+    init_func=init,
+    interval=36000,
+    blit=True
+)
+
+plt.show()
