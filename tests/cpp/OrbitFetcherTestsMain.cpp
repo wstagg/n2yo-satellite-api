f8e0d5d96c49648d4009e321c87271591311d375
wesley.stagg <wesley.stagg@uk.thalesgroup.com>
2025-12-10 14:56:43 +0000

    changes to CMakeLists.txt for windows building

--- tests/cpp/OrbitFetcherTestsMain.cpp
+++ tests/cpp/OrbitFetcherTestsMain.cpp
@@ -1,3 +1,8 @@
-// tests/cpp/test_main.cpp
-#define BOOST_TEST_MODULE n2yoSatelliteApiTests
-#include <boost/test/included/unit_test.hpp>
+// OrbitFetcherTestsMain.cpp
+#define BOOST_TEST_MODULE OrbitFetcherTests
+
+#ifdef _WIN32
+#include <boost/test/unit_test.hpp>  // Compiled library on Windows
+#else
+#include <boost/test/included/unit_test.hpp>  // Header-only on Linux
+#endif
